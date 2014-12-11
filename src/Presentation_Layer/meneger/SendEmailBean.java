af47275e8ce2ed575688601de89facc601b17014
JonySeed <kamko_a@mail.ru>
2014-12-11 23:20:53 +0200

    доделал рассылку сообщений на email

--- src/Presentation_Layer/meneger/SendEmailBean.java
+++ src/Presentation_Layer/meneger/SendEmailBean.java
@@ -1,8 +1,12 @@
 package PresentationLayer.meneger;
 
+import integration_layer.SenderTls;
+
 import java.util.ArrayList;
 
 import javax.faces.bean.ManagedBean;
+
+import service_layer.MailSender;
 
 @ManagedBean
 public class SendEmailBean {
@@ -11,26 +15,43 @@ public class SendEmailBean {
 	private String role;
 	private ArrayList<ClientFromList> listUsers = new ArrayList<ClientFromList>();
 	private String message;
-	
+	private String subjectMess;
+
+	public String getSubjectMess() {
+		return subjectMess;
+	}
+
+	public void setSubjectMess(String subjectMess) {
+		this.subjectMess = subjectMess;
+	}
 
 	public void selectUsersList(){
 		
 		System.out.println("выполняем выборку");
 		//временно
-		listUsers.add(new ClientFromList(1, "Ваня Дурачек", "Клиент", 5,"test1@test"));
-		listUsers.add(new ClientFromList(2, "Васиа Кавказ", "Клиент",4,"test2@test"));
-		listUsers.add(new ClientFromList(3, "Светка Соколова", "Клиент",3,"test3@test"));
-		listUsers.add(new ClientFromList(4, "Просковья Афанаcьева", "Консультант",2,"test4@test"));
-		listUsers.add(new ClientFromList(5, "Федька Заводила", "Консультант",1,"test5@test"));
+		/*listUsers.add(new ClientFromList(1, "Ваня Дурачек", "Клиент", 5,"test1@test"));
+		listUsers.add(new ClientFromList(2, "Васиа Кавказ", "Клиент",4,"test2@test"));*/
+		listUsers.add(new ClientFromList(3, "Светка Соколова", "Клиент",3,"maxkomar65@gmail.com"));
+		listUsers.add(new ClientFromList(4, "Просковья Афанаcьева", "Консультант",2,"maxkomar65@gmail.com"));
+		listUsers.add(new ClientFromList(5, "Федька Заводила", "Консультант",1,"maxkomar65@gmail.com"));
+		
+
 		
 		//вызвать функцию из сервисов и передать параметры
 		
 	}
 
 	public void sendEmails(){
-		System.out.println("отправка сообщений");
 		
-		namberOfAccounts = 10;
+		selectUsersList();
+		
+
+		//MailSender.send(subjectMess, message, "maxkomar65@gmail.com");
+		for (ClientFromList clientFromList : listUsers) {
+			MailSender.send(subjectMess, message, clientFromList.getEmail());
+		}
+		System.out.println("отправка сообщений");
+		//namberOfAccounts = 10;
 	}
 	
 	public int getNamberOfAccounts() {
